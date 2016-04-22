9c2fb2e7bee49231de8556aeb05a14c4f1b633d7
Saurutobi <saurutobi@gmail.com>
2016-04-21 21:39:00 -0700

    Roll-a-Ball Tutorial

--- Roll-a-Ball/Assets/Scripts/PlayerController.cs
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody rigidBody;
+	private int count;
 
 	void Start()
 	{
 		rigidBody = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText();
+		winText.text = string.Empty;
 	}
 
 	void FixedUpdate()
@@ -17,8 +24,27 @@ public class PlayerController : MonoBehaviour
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
 
-		Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
 		rigidBody.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+		if(count >= 10)
+		{
+			winText.text = "You fucking win!";
+		}
 	}
 }
